a6c5aeaa6e2856aea6f54a85049ceae5ab4a2972
Alexis <meunier_33@ive.fr>
2019-02-02 16:23:51 +0100

    menu maxi bestof frite éviant

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,41 +1,64 @@
-if(document.querySelector('.logo-language'))
+// if(document.querySelector('.logo-language'))
+// {
+//     let logoLanguage = document.querySelector('.logo-language');
+//     let menu = document.querySelector('.submenu');
+//     let show = false;
+
+//     logoLanguage.addEventListener('click', function()
+//     {
+//         if(show)
+//         {
+//             menu.style.display = 'none';
+//             show = false;
+//         }
+//         else
+//         {
+//             menu.style.display = 'inline-block';
+//             show = true;
+//         }
+//     });
+// }
+
+// if(document.querySelector('.span-registration'))
+// {
+//     let spanRegistration = document.querySelector('.span-registration');
+//     let registration = document.querySelector('.submenu1');
+//     let show = false;
+
+//     spanRegistration.addEventListener('click', function()
+//     {
+//         if(show)
+//         {
+//             registration.style.display = 'none';
+//             show = false;
+//         }
+//         else
+//         {
+//             registration.style.display = 'inline-block';
+//             show = true;
+//         }
+//     });
+// }
+
+if(document.querySelectorAll('.btn-submenu'))
 {
-    let logoLanguage = document.querySelector('.logo-language');
-    let menu = document.querySelector('.submenu');
+    let btns = document.querySelectorAll('.btn-submenu');
     let show = false;
 
-    logoLanguage.addEventListener('click', function()
-    {
+    btns.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+        console.log(e);
         if(show)
         {
-            menu.style.display = 'none';
+            e.target.parentElement.parentElement.children[1].style.display = "none";
             show = false;
         }
         else
         {
-            menu.style.display = 'inline-block';
+            e.target.parentElement.parentElement.children[1].style.display = "initial";
             show = true;
         }
-    });
-}
-
-if(document.querySelector('.span-registration'))
-{
-    let spanRegistration = document.querySelector('.span-registration');
-    let registration = document.querySelector('.submenu1');
-    let show = false;
-
-    spanRegistration.addEventListener('click', function()
-    {
-        if(show)
-        {
-            registration.style.display = 'none';
-            show = false;
-        }
-        else
-        {
-            registration.style.display = 'inline-block';
-            show = true;
-        }
+            
+        });
     });
 }
